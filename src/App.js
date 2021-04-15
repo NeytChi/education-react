90c5161feed996b914178419c8ee39d4a3f00ecb
NeytChi <neyton61@gmail.com>
2021-04-15 16:06:37 +0300

    INF: Change init export in components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import GreetCmpt from './components/Greet'
+import { Greet } from './components/Greet'
 
 class App extends Component
 {
   render() {
     return (
       <div className="App">
-        <GreetCmpt />
+        <Greet />
       </div>
     );
   }
