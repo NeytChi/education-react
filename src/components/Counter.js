3c05d6380a99d1f16b77fd6b6dc864ec5bc4385a
NeytChi <neyton61@gmail.com>
2021-04-15 17:47:29 +0300

    INF: Create Counter with setState example

---
+++ src/components/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Counter extends Component
+{
+    constructor() {
+        super()
+        this.state = {
+            counter : 0
+        }
+    }
+    count() {
+        this.setState ({
+            counter : this.state.counter + 1
+        },
+        () => {
+            console.log('Callback value', this.state.counter)
+        })
+        console.log('Callback value', this.state.counter);
+    }
+    render()
+    {
+        return (
+        <div>
+            <h1>Counter = '{this.state.counter}'</h1>
+            <button onClick={() => this.count()} >Up!</button>
+        </div>
+        )
+    }
+}
+
+export default Counter
