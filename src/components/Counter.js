7ae96fa1a5adda2d85132065c2f55846d8c7c503
NeytChi <neyton61@gmail.com>
2021-04-15 17:52:08 +0300

    INF: Create Counter set prevState in SetState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,9 +9,9 @@ class Counter extends Component
         }
     }
     count() {
-        this.setState ({
-            counter : this.state.counter + 1
-        },
+        this.setState ((prevState, props) => ({
+            counter : prevState.counter + 1
+        }),
         () => {
             console.log('Callback value', this.state.counter)
         })
