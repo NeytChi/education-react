f00b3a7faec240df3b47a78b789838bf33d44683
NeytChi <neyton61@gmail.com>
2021-04-17 16:46:37 +0300

    INF: Using fragments

---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FragmentDemo() {
+    return (
+        <React.Fragment>
+            <h1>Fragment Demo</h1>
+            <p>This describes a Fragment Demo component!</p>
+        </React.Fragment>
+    )
+}
