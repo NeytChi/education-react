a4929271f1c2e08ee447a2adeda666254940d5c9
NeytChi <neyton61@gmail.com>
2021-04-17 17:32:46 +0300

    INF: Using Pure components

--- src/App.js
+++ src/App.js
@@ -20,13 +20,16 @@ import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
 
 class App extends Component
 {
   render() {
     return (
       <div className="App">
-          <Table />
+        <ParentComp />
+          {/*<Table />*/}
           {/*<FragmentDemo />*/}
           {/*<LifecycleA />*/}
           {/*<h1 className='error'>Error</h1>*/}
